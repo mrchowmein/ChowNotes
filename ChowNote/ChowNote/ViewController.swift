1752fb7cde7318b68c896876be607193f5ca4322
Jason Chan <jasonfchan@gmail.com>
2016-02-03 19:09:57 -0800

    added detection

--- ChowNote/ChowNote/ViewController.swift
+++ ChowNote/ChowNote/ViewController.swift
@@ -9,12 +9,12 @@
 import UIKit
 import CoreData
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate {
 
     @IBOutlet weak var titleName: UITextField!
     
     
-    @IBOutlet weak var bodyText: UITextField!
+    @IBOutlet weak var bodyText: UITextView!
     
     
     var note: NoteEntity? = nil
@@ -28,13 +28,58 @@ class ViewController: UIViewController, UITextFieldDelegate {
             titleName.text = note?.title
         }
         
+        let tap = UITapGestureRecognizer(target: self, action: "tapDetected")
+        tap.numberOfTapsRequired = 1
+        self.view.addGestureRecognizer(tap)
+        self.bodyText.editable = false
+        bodyText.delegate = self
+        
+        titleName.delegate = self
+        
+        //bodyText placeholder text
+        if bodyText.text.isEmpty {
+            bodyText.text = "Note"
+            bodyText.textColor = UIColor.lightGrayColor()
+        }
     }
 
+//Mark: - bodyText Placeholder text changes
+    
+    func textViewDidBeginEditing(bodyText: UITextView) {
+        if bodyText.textColor == UIColor.lightGrayColor() {
+            bodyText.text = nil
+            bodyText.textColor = UIColor.blackColor()
+        }
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+//Mark: - Gesture to change textview edit state
+    func tapDetected() {
+        
+        self.bodyText.editable = true
+        print("test")
+    }
+    
+    
+    func textViewDidEndEditing(textView: UITextView) {
+        
+        self.bodyText.editable = false
+        self.bodyText.dataDetectorTypes = UIDataDetectorTypes.All
+        
+        if bodyText.text.isEmpty {
+            bodyText.text = "Enter Note"
+            bodyText.textColor = UIColor.lightGrayColor()
+        }
+
+    }
+    
+    
+    
     @IBAction func cancelButton(sender: AnyObject) {
         
         
@@ -145,11 +190,29 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
     
 // Mark: - dismiss keyboard
+
+    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+        if(text == "\n") {
+            bodyText.resignFirstResponder()
+            return false
+        }
+        return true
+    }
     
+    func textFieldShouldReturn(textField: UITextField) -> Bool  {
+        titleName.resignFirstResponder()
+        return true;
+    }
+    
+    
+    
+    /*
 override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?){
        view.endEditing(true)
+    bodyText.editable = false
     super.touchesBegan(touches, withEvent: event)
     }
+*/
 
 }
 
